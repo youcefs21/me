ed852314d4ac03747ec4d6eb14cf6c5894ec0bf1
youcefs21 <youcefs2121@gmail.com>
2022-05-11 11:05:34 -0400

    styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,30 +1,45 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
 
-const pageStyles = {
-  color: "#232129",
-  backgroundColor: "#f6f6f6",
-  padding: 50,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
 
 type LayoutProps = {
   pageTitle: string,
   children: JSX.Element
 }
 
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className={navLinks}>
+        <li className={navLinkItem}>
+          <Link to="/" className={navLinkText}>
+            Home
+          </Link>
+        </li>
+        <li className={navLinkItem}>
+          <Link to="/about" className={navLinkText}>
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
 const Layout = ({ pageTitle, children }: LayoutProps) => {
   return (
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main style={pageStyles}>
-        <h1>{pageTitle}</h1>
+      <NavBar/>
+      <main style={{padding: 20}}>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
