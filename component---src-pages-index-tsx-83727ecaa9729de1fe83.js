30ff72cd1d8dfc212a52839221028e2350392f4c
youcefs21 <youcefs2121@gmail.com>
2022-05-22 19:51:38 -0400

    Updates

---
+++ component---src-pages-index-tsx-83727ecaa9729de1fe83.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[691],{6854:function(e,n,t){t.r(n);var o=t(7294),l=t(9762);n.default=function(){return o.createElement(l.Z,{pageTitle:"Youcef",pageDesc:"I am software engineer who loves learning new things and exploring new technologies"},o.createElement("div",null,"test 2"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-83727ecaa9729de1fe83.js.map
