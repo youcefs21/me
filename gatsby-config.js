08ef232392141dbc63c4a8dc885c4e9c2c983840
youcefs21 <youcefs2121@gmail.com>
2022-05-24 18:04:34 -0400

    moving nav bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,19 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet"
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#21212F`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/images/myIcon.png'
+      },
+    },
   ],
   pathPrefix: "/me",
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from "../components/layout";
+import {Helmet} from "react-helmet";
 
 
 const IndexPage = () => {
   return (
       <Layout pageTitle={"Youcef"} pageDesc={"I am software engineer who loves learning new things and exploring new technologies"}>
         <div>
+          <Helmet>
+            <title>Youcef</title>
+            <meta name="description" content="portfolio page" />
+          </Helmet>
         </div>
       </Layout>
   )
