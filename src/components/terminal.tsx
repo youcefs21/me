63c3e29bf6f28162a90fbf42dd49d6a3548cb3ae
youcefs21 <youcefs2121@gmail.com>
2022-05-26 11:02:05 -0400

    terminal typing

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,14 +1,47 @@
 import * as React from "react";
 import '../styles/terminal.css'
 
-const Terminal = () => {
-  return (
-    <div className={"box"}>
-      <div className="terminal-text">
-        <b className="terminal-cursor">█</b>
+class Terminal extends React.Component<{}, { inputValue: string }> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className={"box"}>
+        <input 
+          value={""} 
+          className="terminal-input" 
+          onKeyDown={evt => this.updateSpecialInput(evt)}
+          onChange={evt => this.updateInputValue(evt)}
+        />
+        <div className="terminal-text">
+          {this.state.inputValue}<b className="terminal-cursor">█</b>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  updateSpecialInput(evt: React.KeyboardEvent<HTMLDivElement>) {
+    const key = evt.key;
+    var val = this.state.inputValue
+    if (key === "Backspace"){
+      val = val.slice(0, -1);
+    }
+    console.log(key)
+    this.setState({inputValue: val})
+  }
+
+  updateInputValue(evt: React.ChangeEvent<HTMLInputElement>) {
+    const val = evt.target.value;
+    this.setState({inputValue: this.state.inputValue + val})
+  }
+
+
 }
+
 
 export default Terminal
