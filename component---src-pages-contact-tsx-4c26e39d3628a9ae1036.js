0fa26326ddd6373a76a0ef79a84a15a551ed2384
youcefs21 <youcefs2121@gmail.com>
2022-05-26 23:31:38 -0400

    Updates

---
+++ component---src-pages-contact-tsx-4c26e39d3628a9ae1036.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[650],{2724:function(e,t,o){o.r(t);var r=o(7294),n=o(9762);t.default=function(){return r.createElement(n.Z,{pageTitle:"Contact Me",pageDesc:"Here is how you reach out to me"},r.createElement("div",{style:{marginTop:2e3,textAlign:"center"}},"Work in Progress"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-tsx-4c26e39d3628a9ae1036.js.map
