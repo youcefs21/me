cf323f4b6e578909cbc54ddb7c6e5c45a840e08a
youcefs21 <youcefs2121@gmail.com>
2022-06-08 14:56:05 -0400

    many pages much wowo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,26 +7,16 @@ import Background from "./background";
 
 type LayoutProps = {
   pageTitle: string,
-  pageDesc: string,
-  children: JSX.Element,
+  children: JSX.Element[],
   theme: string
 }
 
-const Layout = ({ pageTitle, pageDesc, children, theme }: LayoutProps) => {
+const Layout = ({ pageTitle, children, theme }: LayoutProps) => {
   return (
     <div className={"container"} id={theme}>
       <title>{pageTitle}</title>
       <NavBar/>
-      <main className={"main background-color1"}>
-        <Background>
-          <div className={"front-page"}>
-            <div>
-              <h1 className={"heading main-text-color"}>{pageTitle}</h1>
-              <h2 className={"sub-heading main-text-color"}>{pageDesc}</h2>
-            </div>
-            <Arrows/>
-          </div>
-        </Background>
+      <main className={"main main-background-color"}>
         {children}
         <div id={'pageBottom'} style={{padding:"1px"}}></div>
       </main>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,60 @@ import * as React from 'react'
 import Layout from "../components/layout";
 import {Helmet} from "react-helmet";
 import Terminal from '../components/terminal';
+import Arrows from "../components/arrows";
+import Background from "../components/background";
+
+
+const FrontPage = ({pageTitle, pageDesc}: {pageTitle: string, pageDesc: string}) => {
+  return (
+    <div className={"front-page"}>
+      <div>
+        <h1 className={"heading main-text-color"}>{pageTitle}</h1>
+        <h2 className={"sub-heading main-text-color"}>{pageDesc}</h2>
+      </div>
+      <Arrows/>
+    </div>
+  )
+}
+
+const IntroPage = () => {
+  return (
+    <div>
+      <FrontPage pageTitle={"Intro Page"} pageDesc={"cool intro"}/>
+    </div>
+  )
+}
+
+const FeaturesPage = () => {
+  return (
+    <div>
+      <FrontPage pageTitle={"Features Page"} pageDesc={"look at all these features"}/>
+    </div>
+  )
+}
 
 
 const IndexPage = () => {
   return (
-      <Layout pageTitle={"Youcef"} pageDesc={"I am a software engineer who loves learning new things and exploring new technologies"} theme={"light"}>
-        <div>
-          <Helmet>
-            <title>Youcef</title>
-            <meta name="description" content="A Portfolio Page" color={"#21212F"}/>
-          </Helmet>
-          <Terminal/>
-        </div>
+      <Layout pageTitle={"Youcef"} theme={"light"}>
+        <Helmet>
+          <title>Youcef</title>
+          <meta name="description" content="A Portfolio Page" color={"#21212F"}/>
+        </Helmet>
+        <Background className={"background-color-1"} zIndex={10}>
+          <FrontPage
+            pageTitle={"Youcef"}
+            pageDesc={"I am a software engineer who loves learning new things and exploring new technologies"}
+          />
+        </Background>
+        <Background className={"background-color-2"} zIndex={9}>
+          <IntroPage/>
+        </Background>
+        <Background className={"background-color-3"} zIndex={8}>
+          <FeaturesPage/>
+        </Background>
+        <h1 className={"heading main-text-color"}>The Terminal</h1>
+        <Terminal/>
       </Layout>
   )
 }
