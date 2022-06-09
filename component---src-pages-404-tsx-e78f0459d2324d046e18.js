0309c377f1444de7f2232396503e0985e1411bdb
youcefs21 <youcefs2121@gmail.com>
2022-06-08 21:40:13 -0400

    Updates

---
+++ component---src-pages-404-tsx-e78f0459d2324d046e18.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[218],{2224:function(e,t,n){n.r(t);var l=n(7294),a=n(6375),c=n(5414);t.default=function(){return l.createElement(a.Z,{pageTitle:"404",pageDesc:"ERROR 404 PAGE NOT FOUND"},l.createElement("div",null,l.createElement(c.q,null,l.createElement("title",null,"404"),l.createElement("meta",{name:"description",content:"page not found"}))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-e78f0459d2324d046e18.js.map
